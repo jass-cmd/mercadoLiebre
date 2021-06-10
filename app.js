2b37040abc35f53766aca4f94e165183f5634e20
jass-cmd <jass.t93@gmail.com>
2021-06-09 22:06:00 -0300

    mercadoliebre

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.use(express.static('public'));
+
+/*servidor */
+
+app.listen(process.env.PORT || 3000, () => console.log('servidor abierto en l puerto 3000'));
+
+/* Rutas*/
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+
+
